4017da0bdf30125804d941aca127d10362cad8b9
Andrew Harvey <andrew.harvey4@gmail.com>
2018-08-09 21:26:42 +1000

    drop minimist dependency from cli

--- cli.js
+++ cli.js
@@ -1,24 +1,41 @@
 #!/usr/bin/env node
 
+'use strict';
+
 const osmGeoJson = require('./index.js');
 const fs = require('fs');
-const argv = require('minimist')(process.argv.slice(2), {
-  alias: { 'h': 'help' },
-  boolean: ['h', 'pretty']
-});
 
-if (argv.h) {
-  console.log('Usage: osm-geojson [options] osmid:filename.geojson...')
-  console.log('Options:')
-  console.log('   -h --help Show usage')
-  console.log('   --pretty Pretty print GeoJSON')
-}
+const argv = { _: [] };
+
+process.argv.slice(2).forEach((arg) => {
+  switch (arg) {
+    case '-h':
+    case '--help':
+      console.log('Usage: osm-geojson [options] osmid:filename.geojson...');
+      console.log('Options:');
+      console.log('   -h --help Show usage');
+      console.log('   -p --pretty-print Pretty print GeoJSON');
+      process.exit();
+      break;
+
+    case '-p':
+    case '--pretty-print':
+      argv['pretty-print'] = true;
+      break;
+
+    default:
+      if (!arg.startsWith('-')) {
+        argv._.push(arg);
+      }
+      break;
+  }
+})
 
 argv._.forEach((task) => {
   let [relation, file] =  task.split(':')
   osmGeoJson.get(relation)
     .then((geojson) => {
-      const geojsonString = argv.pretty ? JSON.stringify(geojson, null, 2) : JSON.stringify(geojson);
+      const geojsonString = argv['pretty-print'] ? JSON.stringify(geojson, null, 2) : JSON.stringify(geojson);
       if (file === '-') {
         process.stdout.write(geojsonString)
       } else {
