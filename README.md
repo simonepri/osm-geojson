2f9f669ab077a60163b4cf32a00136c6e36dfc77
simonepri <simonepri@outlook.com>
2018-08-10 11:50:26 +0100

    Improve CLI

--- README.md
+++ README.md
@@ -87,6 +87,21 @@ osmGeoJson.getAll({'ITA': '365331', 'USA': '148838'}); // Italy
 // }
 ```
 
+## CLI
+
+```bash
+Usage
+  $ npx osm-geojson <osmId>
+Options
+  -p --pretty                   To beautify the output.
+  -l --list                     To provide a list of osmId.
+Examples
+  $ npx osm-geojson 365331
+  $ npx osm-geojson -p 365331
+  $ npx osm-geojson -p 365331 >> filename.geojson
+  $ npx osm-geojson -l 365331:ita 148838:usa
+```
+
 ## API
 
 <a name="get"></a>
@@ -112,12 +127,6 @@ of the map provided but with the GeoJSON of the given relation id as value.
 | Param | Type | Description |
 | --- | --- | --- |
 | map | <code>object</code> | Map from a name to a relation id from which extract the GeoJSON. |
-
-## CLI
-
-```
-$ osm-geojson osmid:file.geojson
-```
 
 ## Authors
 
--- cli.js
+++ cli.js
@@ -3,50 +3,112 @@
 'use strict';
 
 const fs = require('fs');
+const pify = require('pify');
+
 const osmGeoJson = require('.');
 
-const argv = {_: []};
-
-process.argv.slice(2).forEach(arg => {
-  switch (arg) {
-    case '-h':
-    case '--help':
-      console.log('Usage: osm-geojson [options] osmid:filename.geojson...');
-      console.log('Options:');
-      console.log('   -h --help Show usage');
-      console.log('   -p --pretty-print Pretty print GeoJSON');
-      process.exit();
-
-    case '-p':
-    case '--pretty-print':
-      argv['pretty-print'] = true;
-      break;
-
-    default:
-      if (!arg.startsWith('-')) {
-        argv._.push(arg);
-      }
-      break;
+function stringify(geojson, beautify) {
+  if (beautify) {
+    return JSON.stringify(geojson, null, 2);
   }
-});
+  return JSON.stringify(geojson);
+}
 
-argv._.forEach(task => {
-  const [relation, file] = task.split(':');
-  osmGeoJson.get(relation).then(
-    geojson => {
-      const geojsonString = argv['pretty-print']
-        ? JSON.stringify(geojson, null, 2)
-        : JSON.stringify(geojson);
-      if (file === '-') {
-        process.stdout.write(geojsonString);
-      } else {
-        fs.writeFile(file, geojsonString, err => {
-          if (err) throw err;
-        });
-      }
-    },
-    err => {
-      console.error(err);
+function help() {
+  const help = ` Usage
+    $ osm-geojson <osmId>
+  Options
+    -p --pretty                   To beautify the output.
+    -l  --list                    To provide a list of osmId.
+  Examples
+    $ osm-geojson 365331
+    $ osm-geojson -p 365331
+    $ osm-geojson -p 365331 >> filename.geojson
+    $ osm-geojson -l 365331:ita 148838:usa
+`;
+  console.log(help);
+
+  return Promise.resolve();
+}
+
+function list(osmidmap, beautify) {
+  const promises = [];
+
+  for (const country of Object.keys(osmidmap)) {
+    promises.push(
+      osmGeoJson
+        .get(osmidmap[country])
+        .then(geojson =>
+          pify(fs.writeFile)(country + '.geojson', stringify(geojson, beautify))
+        )
+    );
+  }
+
+  return Promise.all(promises);
+}
+
+function get(osmid, beautify) {
+  return osmGeoJson.get(osmid).then(geojson => {
+    console.log(stringify(geojson, beautify));
+  });
+}
+
+function run() {
+  const argv = {_: []};
+
+  // Parse flags
+  process.argv.slice(2).forEach(arg => {
+    switch (arg) {
+      case '-p':
+      case '--pretty-print':
+        argv.beautify = true;
+        break;
+      case '-l':
+      case '--list':
+        argv.list = true;
+        break;
+
+      default:
+        if (!arg.startsWith('-')) {
+          argv._.push(arg);
+        }
+        break;
     }
-  );
-});
+  });
+
+  // Validate argv list
+  if (argv.list) {
+    argv._ = argv._.filter(arg => /\d+:.+/.test(arg));
+    if (argv._.length === 0) {
+      argv.help = true;
+    }
+  } else {
+    argv._ = argv._.filter(arg => /\d+/.test(arg));
+    if (argv._.length !== 1) {
+      argv.help = true;
+    }
+  }
+
+  // Choose the right handler
+  return Promise.resolve()
+    .then(() => {
+      if (argv.help) {
+        return help();
+      }
+      if (argv.list) {
+        const map = {};
+        argv._.forEach(arg => {
+          const [osmid, country] = arg.split(':');
+          map[country] = osmid;
+        });
+        return list(map, argv.beautify);
+      }
+      const osmid = argv._[0];
+      return get(osmid, argv.beautify);
+    })
+    .catch(err => {
+      console.error('\n❌ ' + err);
+    });
+}
+
+run();
